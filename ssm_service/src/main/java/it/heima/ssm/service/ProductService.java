a8efd93a5177116bc5c5cdb2a7927df6cee492c1
WarMj <R.Donghui@outlook.com>
2020-03-10 15:54:16 +0800

    实现Product的service接口 并去掉service接口中的public修饰

---
+++ ssm_service/src/main/java/it/heima/ssm/service/ProductService.java
@@ -0,0 +1,11 @@
+package it.heima.ssm.service;
+
+import com.itheima.ssm.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    //查询所有的产品信息
+    List<Product> findAll() throws Exception;
+}
